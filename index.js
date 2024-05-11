f15a3621e4478eca9e9bed468de23b22bda7b6e6
vishakh-cs <143394085+vishakh-cs@users.noreply.github.com>
2024-05-11 21:14:56 +0530

    update

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ import UserRoutes from "./routes/UserRoute.js";
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: 'https://fit-tone.vercel.app'
-}));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); 
 
