6e363dc19afd0addc2a451f9d744d2068bfb8a6f
vishakh-cs <143394085+vishakh-cs@users.noreply.github.com>
2024-05-11 20:45:36 +0530

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); 
 
 const corsOptions = {
-  origin: '*',
+  origin: 'https://fit-tone.vercel.app',
   methods: 'GET,POST',
   allowedHeaders: 'Content-Type,Authorization',
 };
