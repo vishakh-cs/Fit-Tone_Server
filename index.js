432da5ffcf18d432fea163a79b2909d12f45c649
vishakh-cs <143394085+vishakh-cs@users.noreply.github.com>
2024-05-11 20:29:23 +0530

    update

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); 
+
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  methods: 'GET,POST',
+  allowedHeaders: 'Content-Type,Authorization',
+};
+app.use(cors(corsOptions));
+
  
 app.use("/", UserRoutes);
 // error handler
